9c7d37c96e21dda361dd3c25c3162b98b8e87f98
Anael <anael.guzman@usach.cl>
2025-09-28 18:22:26 -0300

    Inicio del proyecto y subida de backend a el repositorio de github

---
+++ Backend/src/test/java/com/backend_gestion_balta/GestionBaltaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_gestion_balta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBaltaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
